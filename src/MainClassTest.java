82335486ad671911cabe6c7dc05cd9c2d9d9beaf
AkimovaOlesya <67740984+AkimovaOlesya@users.noreply.github.com>
2021-04-26 23:12:00 +0700

    Test2
    Выполнено задание из Тест 2

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,8 +1,9 @@
+import org.junit.Assert;
 import org.junit.Test;
 
-public class MainClassTest extends MainClass{
+public class MainClassTest{
     MainClass main = new MainClass();
-
+    //Тест 1
     @Test
     public void testGetLocalNumber(){
         int number = main.getLocalNumber();
@@ -10,6 +11,16 @@ public class MainClassTest extends MainClass{
             System.out.println("Method return true number");
         }else {
             System.out.println("This method return false number");
+        }
+    }
+    //Тест 2
+    @Test
+    public void testGetClassNumber(){
+        int number = main.getClassNumber();
+        if (number > 45){
+            Assert.assertTrue("The Method getClassNumber return number > 45", true );
+        } else {
+            Assert.fail("The Method getClassNumber return number < 45");
         }
     }
 }
