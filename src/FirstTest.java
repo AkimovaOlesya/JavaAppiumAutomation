5234018fa3cf723f8f67507943200ea1a08633e0
oakimova <o.akimova@cft.ru>
2021-06-04 18:58:30 +0700

    06. Assert: assertion error

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -483,6 +483,37 @@ public class FirstTest {
         );
     }
 
+    @Test
+    public void testAmountOfEmptySearch(){
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find 'Search Wikipedia' input",
+                5
+        );
+
+        String search_line = "xjdjchdj";
+        waitForElementAndSendKeys(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]") ,
+                search_line,
+                "Cannot find search input",
+                5
+        );
+
+        String search_result_locator = "//*[@resource-id='org.wikipedia:id/search_results_list']/*[@resource-id='org.wikipedia:id/page_list_item_container']";
+        String empty_result_label = "//*[@text='No results found']";
+
+        waitForElementPresent(
+                By.xpath(empty_result_label),
+                "Cannot find empty result label by the request " + search_line,
+                15
+        );
+
+        assertElementNotPresent(
+                By.xpath(search_result_locator),
+                "We've found some results by request " + search_line
+        );
+    }
+
     private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds){
         WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
         wait.withMessage(error_message + "\n");
@@ -598,5 +629,14 @@ public class FirstTest {
     private int getAmountOfElements(By by){
      List elements = driver.findElements(by);
      return elements.size();
+    }
+
+    private void assertElementNotPresent(By by, String error_message){
+     int amountOfElements = getAmountOfElements(by);
+     if (amountOfElements > 0){
+         String default_message = "An element '" + by.toString() + "' supposed to be not present";
+         throw new AssertionError(default_message + " " + error_message);
+
+     }
     }
 }
